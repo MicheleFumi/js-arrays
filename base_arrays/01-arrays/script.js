f16d3ba58fb315705357cc4243dd952f60f71097
MicheleFumi <michele.fumi96@gmail.com>
2024-10-02 15:25:20 +0200

    add more solutions

--- base_arrays/01-arrays/script.js
+++ base_arrays/01-arrays/script.js
@@ -10,27 +10,35 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = teachers.indexOf("Phil");
+const fourthTeacher = teachers.indexOf("Carlo");
 console.log(fourthTeacher);
 
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
-const fifth_teacher = teachers.indexOf("Carlo")
+const fifth_teacher = teachers.indexOf("Lewis")
 if (fifth_teacher !== -1){
     teachers.splice(fifth_teacher, 1, "Patrick"); 
 }
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+console.log(teachers);
+
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(teachers);
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push ("Vanessa");
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift("Sarah");
+console.log(teachers);
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
