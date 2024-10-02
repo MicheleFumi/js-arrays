b5b5e755466f87a0dc50d0142b2848d1aa8d2d01
MicheleFumi <michele.fumi96@gmail.com>
2024-10-02 17:24:24 +0200

    add first two bonus and fix first part script

--- base_arrays/01-arrays/script.js
+++ base_arrays/01-arrays/script.js
@@ -59,14 +59,7 @@ console.log(teachersString);
 
 // 10. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty= true;
-if(teachers.length === 0){
-  console.log(isTeachersEmpty);
+const isTeachersEmpty= teachers.length === 0;
+console.log(isTeachersEmpty);
   
-} else{
-  console.log(false);
-  
-}
-
-
 
